2d28bf80946f34c3bd3d36c255ea2cf0d2c19c31
Cosmin Ronnin <coszmin@gmail.com>
2015-03-28 22:49:21 -0400

    release: 0.0.0

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import {Router} from 'aurelia-router';
+import bootstrap from 'bootstrap';
+
+export class App {
+  static inject() { return [Router]; }
+  constructor(router) {
+    this.router = router;
+    this.router.configure(config => {
+      config.title = 'Aur';
+      config.map([{ 
+        route: ['','welcome'],  
+        moduleId: './welcome',      
+        nav: true, 
+        title:'Welcome' 
+      }
+      ]);
+    });
+  }
+}
