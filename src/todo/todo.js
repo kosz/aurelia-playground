1e0a2c405dc53a461176a0f7aed06dc5411b0f69
Cosmin Ronnin <coszmin@gmail.com>
2015-04-01 16:01:25 -0400

    dev(ngRepeat): repeat.for example, equivalent of ng-reapeat

--- src/todo/todo.js
+++ src/todo/todo.js
@@ -3,5 +3,9 @@
 export class Todo{
   //static inject() { return [HttpClient,CustomService]; }
   constructor(){
+    this.todos = [
+      { text: 'Todo 1', done: false },
+      { text: 'Todo 2', done: false }
+    ];
   }
 }
