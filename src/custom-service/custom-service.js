9f7858d32455ed66a3e42972c53ae91b1f0ed971
Cosmin Ronnin <coszmin@gmail.com>
2015-04-01 15:03:19 -0400

    research(instances): how to create 'services' as singletons or new instances

--- src/custom-service/custom-service.js
+++ src/custom-service/custom-service.js
@@ -1,4 +1,7 @@
+//import {Metadata} from 'aurelia-framework';
+
 export class CustomService {
+  //static metadata(){ return Metadata.transient(); }
   constructor() {
     this.globalValue = 'Johnson';
   }
